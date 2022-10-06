ac8593f1c870a50b860c88b5a76e29096bb26189
YooDongseon <nick258525@gmail.com>
2022-10-06 23:17:14 +0900

    #2

--- IWATCHU.py
+++ IWATCHU.py
@@ -1,7 +1,9 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
-
+from PyQt5 import QtCore
+from PyQt5.QtCore import Qt, pyqtSlot
+from PyQt5.Qt import QFileSystemModel
 
 class MyApp(QMainWindow):
 
@@ -13,7 +15,6 @@ class MyApp(QMainWindow):
         self.setWindowTitle('WATCH Analyzer')   # 창 띄우기
         self.setWindowIcon(QIcon('watch.png'))    # 어플리케이션 아이콘 넣기
         self.setGeometry(300, 150, 1280, 720)
-        self.show()
 
         loadAction = QAction('load image file', self)
         loadAction.triggered.connect(self.Fileload)
@@ -23,7 +24,7 @@ class MyApp(QMainWindow):
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(qApp.quit)
 
-        # 메뉴바 만들기
+        # 메뉴바 만들기 -----------------------------------------------------
         menubar = self.menuBar()
         menubar.setNativeMenuBar(False)
         filemenu = menubar.addMenu('&File')
@@ -36,6 +37,53 @@ class MyApp(QMainWindow):
         toolsmenu = menubar.addMenu('&Tools')
         settingsmenu = menubar.addMenu('&Settings')
         helpmenu = menubar.addMenu('&Help')
+
+        # 창 나누기 ---------------------------------------------------------
+        left = QFrame()
+        left.setFrameShape(QFrame.Box)
+        # left.setFrameShadow(QFrame.Sunken)
+
+        right = QFrame()
+        right.setFrameShape(QFrame.Box)
+
+        bottom = QFrame()
+        bottom.setFrameShape(QFrame.Box)
+
+        self.splitter1 = QSplitter(Qt.Vertical, self)
+        self.splitter1.addWidget(right)
+        self.splitter1.addWidget(bottom)
+
+        self.splitter2 = QSplitter(Qt.Horizontal, self)
+        self.splitter2.setGeometry(10, 40, 1260, 670)
+        self.splitter2.addWidget(left)
+        self.splitter2.addWidget(self.splitter1)
+
+        self.splitter1.setSizes([200, 100])
+        self.splitter2.setSizes([100, 400])
+
+        # 파일 트리뷰 --------------------------------------------------------
+        self.path_root = QtCore.QDir.rootPath() # 기본 C 드라이브 경로... 추후 수정
+        self.model = QFileSystemModel()
+        self.model.setRootPath(self.path_root)
+
+        self.index_root = self.model.index(self.model.rootPath())
+
+        self.tree_view = QTreeView()
+        self.tree_view.setModel(self.model)
+        self.tree_view.setRootIndex(self.index_root)
+        self.tree_view.clicked.connect(self.on_treeView_clicked)
+
+        # self.setCentralWidget(self.tree_view)
+
+    @pyqtSlot(QtCore.QModelIndex)
+    def on_treeView_clicked(self, index):
+        indexItem = self.model.index(index.row(), 0, index.parent())
+
+        fileName = self.model.fileName(indexItem)
+        filePath = self.model.filePath(indexItem)
+
+        print(fileName)
+        print(filePath)
 
     def Fileload(self):
         fname = QFileDialog.getOpenFileName(self, 'Open file', './')
@@ -50,4 +98,5 @@ class MyApp(QMainWindow):
 if __name__ == '__main__':
    app = QApplication(sys.argv)
    ex = MyApp()
+   ex.show()
    sys.exit(app.exec_())
